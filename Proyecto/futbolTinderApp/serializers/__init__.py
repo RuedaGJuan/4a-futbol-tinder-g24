c9c48023f941fad214d65974a4b58e6958812d96
lipasofra <lipasofra.ph@gmail.com>
2021-11-16 14:47:44 -0500

    nueva carpeta authMS y migraciones

--- Proyecto/futbolTinderApp/serializers/__init__.py
+++
@@ -1,4 +0,0 @@
-from .userSerializer import UserSerializer
-from .equipoSerializer import EquipoSerializer
-from .convocatoriaSerializer import ConvocatoriaSerializer
-from .jugadorSerializer import JugadorSerializer
