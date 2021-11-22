99d71aa03f3a1fe7a91d74cd05babf15ed6a60fe
lipasofra <lipasofra.ph@gmail.com>
2021-11-22 18:44:32 -0500

    creacion proyecto de Spring

---
+++ Proyecto/jugadorMS/src/main/java/com/FutMatch/jugadorMS/JugadorMsApplication.java
@@ -0,0 +1,13 @@
+package com.FutMatch.jugadorMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JugadorMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JugadorMsApplication.class, args);
+	}
+
+}
---
+++ Proyecto/jugadorMS/src/test/java/com/FutMatch/jugadorMS/JugadorMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FutMatch.jugadorMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JugadorMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
